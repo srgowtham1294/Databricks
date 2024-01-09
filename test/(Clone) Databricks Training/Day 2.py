dbf0676d8d0a4045f6195aa760cf4bdd219eb71b
srgowtham1294 <srgowtham1294>
2024-01-09 09:29:24 +0000

    initial commit

---
+++ test/(Clone) Databricks Training/Day 2.py
@@ -0,0 +1,25 @@
+# Databricks notebook source
+print("Hello Python world!")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC Select 100
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC println("Hello")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC #Getting Started
+# MAGIC ## Databricks
+# MAGIC ### Delta Lake
+
+# COMMAND ----------
+
+
